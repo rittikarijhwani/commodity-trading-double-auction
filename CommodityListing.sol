150d35b75150af1fbc663934ce6141ca3810123c
Rittika Rijhwani <92781929+rittikarijhwani@users.noreply.github.com>
2025-02-05 18:42:11 +0530

    added commands

--- CommodityListing.sol
+++ CommodityListing.sol
@@ -5,62 +5,76 @@ pragma solidity ^0.8.19;
 import "./UserManagement.sol";
 
 
-contract CommodityListing {
-    struct Commodity {
-        uint id;
-        string name;
-        uint quantity;
-        uint basePrice;
-        address seller;
-        bool isSold;
+contract CommodityListing 
+{
+    struct Commodity 
+    {
+        uint id; //commodity ID
+        string name; //commodity name, set by seller
+        uint quantity; //available amount, set by seller
+        uint basePrice; //minimum price, set by seller
+        address seller; //seller address
+        bool isSold; //to check if it has been sold
     }
 
 
-    mapping(uint => Commodity) public commodities;
-    uint public commodityCount;
-    UserManagement public userManagement;
+    mapping(uint => Commodity) public commodities; //stores commodity as dictionary, using ID as key
 
+    uint public commodityCount; //tracks total number of commodities listed
 
-    event CommodityListed(uint indexed id, string name, uint quantity, uint basePrice, address seller);
-    event CommoditySold(uint indexed id, address buyer);
+    UserManagement public userManagement; //used to store an instance of user management contract to verify roles
 
+    event CommodityListed(uint indexed id, string name, uint quantity, uint basePrice, address seller); //when seller lists a commodity
 
-    constructor(address _userManagement) {
+    event CommoditySold(uint indexed id, address buyer); //when auctioneer marks commodity as sold
+    //CHANGE BUYER TO AUCTIONEER AS BUYER DOES NOT INTERACT WITH THIS FUNCTION^
+
+    constructor(address _userManagement) 
+    {//to get address of deployed user management contract when this contract is deployed (to connect them basically)
         userManagement = UserManagement(_userManagement);
     }
 
 
-    modifier onlySeller() {
-        require(userManagement.getUserRole(msg.sender) == UserManagement.Role.Seller, "Not a registered seller.");
-        _;
+    modifier onlySeller() //verifies using user management contract if function caller is a seller
+    {
+        require(userManagement.getUserRole(msg.sender) == UserManagement.Role.Seller, "Not a registered seller."); //does not execute function body
+        _; //executes function body after role check is complete (if the caller is a registered seller)
     }
 
+    //modifiers are basically attached to other functions to make sure only authorised roles can call those functions
 
-    modifier onlyAuctioneer() {
+    modifier onlyAuctioneer() //verifies using user management contract if function caller is an auctioneer
+    { //same working as onlySeller
         require(userManagement.getUserRole(msg.sender) == UserManagement.Role.Auctioneer, "Not an auctioneer.");
         _;
     }
 
 
-    function listCommodity(string memory _name, uint _quantity, uint _basePrice) external onlySeller {
-        commodityCount++;
-        commodities[commodityCount] = Commodity(commodityCount, _name, _quantity, _basePrice, msg.sender, false);
-        emit CommodityListed(commodityCount, _name, _quantity, _basePrice, msg.sender);
+    function listCommodity(string memory _name, uint _quantity, uint _basePrice) external onlySeller 
+    { //first ensures caller is seller
+        commodityCount++; 
+        commodities[commodityCount] = Commodity(commodityCount, _name, _quantity, _basePrice, msg.sender, false); 
+        //lists commodity (stores in commodity mapping) by putting ID as key and rest of the features as values^, commodity count is used as ID
+        
+        emit CommodityListed(commodityCount, _name, _quantity, _basePrice, msg.sender); //triggers event
     }
 
 
-    function markAsSold(uint _commodityId) external onlyAuctioneer {
-        require(commodities[_commodityId].id != 0, "Commodity does not exist.");
-        require(!commodities[_commodityId].isSold, "Commodity already sold.");
+    function markAsSold(uint _commodityId) external onlyAuctioneer 
+    { //first ensures caller is auctioneer
+        require(commodities[_commodityId].id != 0, "Commodity does not exist."); //checks if commodity exists by checking key in mapping(dictionary)
+        require(!commodities[_commodityId].isSold, "Commodity already sold."); //checks if commodity is already sold
 
 
-        commodities[_commodityId].isSold = true;
-        emit CommoditySold(_commodityId, msg.sender);
+        commodities[_commodityId].isSold = true; //marks as sold if commodity exists and is not sold yet
+
+        emit CommoditySold(_commodityId, msg.sender); //triggers event
     }
 
 
-    function getCommodity(uint _id) external view returns (Commodity memory) {
-        require(commodities[_id].id != 0, "Invalid commodity Id.");
-        return commodities[_id];
+    function getCommodity(uint _id) external view returns (Commodity memory) 
+    { //to get commodity information (structure)
+        require(commodities[_id].id != 0, "Invalid commodity Id."); //checks for existance
+        return commodities[_id]; //returns values of key (ID)
     }
 }
