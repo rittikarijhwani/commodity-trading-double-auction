18fa124a05653a85800884060c62334c164a6430
Rittika Rijhwani <92781929+rittikarijhwani@users.noreply.github.com>
2025-02-05 18:42:41 +0530

    added commands

--- UserManagement.sol
+++ UserManagement.sol
@@ -2,24 +2,27 @@
 pragma solidity ^0.8.19;
 
 
-contract UserManagement {
-    enum Role { None, Buyer, Seller, Auctioneer }
+contract UserManagement 
+{
+    enum Role { None, Buyer, Seller, Auctioneer } //4 types of roles
+    //None = 0, Buyer = 1, Seller = 2, Auctioneer = 3
 
+    mapping(address => Role) public userRoles; //basically dictionary where key = user address and value = role
+    //stores role assigned to each user^, public because referenced in other contracts
 
-    mapping(address => Role) public userRoles;
+    event UserRegistered(address indexed user, Role role); //kinda declares user acc and role to front-end and blockchain
+    //indexed to make it easier to track^
 
-
-    event UserRegistered(address indexed user, Role role);
-
-
-    function registerUser(address _user, Role _role) external {
-        require(userRoles[_user] == Role.None, "User already registered.");
-        userRoles[_user] = _role;
-        emit UserRegistered(_user, _role);
+    function registerUser(address _user, Role _role) external //registers a user with a role and prevents duplicates
+    {
+        require(userRoles[_user] == Role.None, "This user is already registered."); //checks for duplicates
+        userRoles[_user] = _role; //assigns role
+        emit UserRegistered(_user, _role); //triggers event
     }
 
 
-    function getUserRole(address _user) external view returns (Role) {
+    function getUserRole(address _user) external view returns (Role) //to check user role
+    {
         return userRoles[_user];
     }
 }
