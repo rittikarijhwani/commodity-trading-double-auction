772a93460cf77e3ed552351ccc0209a0adc92d09
Rittika Rijhwani <92781929+rittikarijhwani@users.noreply.github.com>
2025-03-05 22:26:14 +0530

    made final changes

--- CommodityListing.sol
+++ CommodityListing.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.19;
 
-
 import "./UserManagement.sol";
-
 
 contract CommodityListing 
 {
@@ -17,7 +15,7 @@ contract CommodityListing
         bool isSold; //to check if it has been sold
     }
 
-
+    //PUT COMMODITY ID WHILE TESTING
     mapping(uint => Commodity) public commodities; //stores commodity as dictionary, using ID as key
 
     uint public commodityCount; //tracks total number of commodities listed
@@ -26,14 +24,17 @@ contract CommodityListing
 
     event CommodityListed(uint indexed id, string name, uint quantity, uint basePrice, address seller); //when seller lists a commodity
 
-    event CommoditySold(uint indexed id, address buyer); //when auctioneer marks commodity as sold
-    //CHANGE BUYER TO AUCTIONEER AS BUYER DOES NOT INTERACT WITH THIS FUNCTION^
+    event CommoditySold(uint indexed id, address auctioneer); //when auctioneer marks commodity as sold
+
+    //mappings for seller sales tracking (dialog boxes content)
+    mapping(address => uint) public totalSalesBySeller; //PUT SELLER ADDRESS WHILE TESTING
+    mapping(address => uint) public itemsSoldBySeller; //PUT SELLER ADDRESS WHILE TESTING
+    mapping(address => uint[]) public sellerListings; //PUT SELLER ADDRESS WHILE TESTING
 
     constructor(address _userManagement) 
     {//to get address of deployed user management contract when this contract is deployed (to connect them basically)
         userManagement = UserManagement(_userManagement);
     }
-
 
     modifier onlySeller() //verifies using user management contract if function caller is a seller
     {
@@ -49,32 +50,45 @@ contract CommodityListing
         _;
     }
 
-
     function listCommodity(string memory _name, uint _quantity, uint _basePrice) external onlySeller 
     { //first ensures caller is seller
         commodityCount++; 
         commodities[commodityCount] = Commodity(commodityCount, _name, _quantity, _basePrice, msg.sender, false); 
         //lists commodity (stores in commodity mapping) by putting ID as key and rest of the features as values^, commodity count is used as ID
         
+        sellerListings[msg.sender].push(commodityCount); //adding to listings for front-end
+        
         emit CommodityListed(commodityCount, _name, _quantity, _basePrice, msg.sender); //triggers event
     }
 
-
-    function markAsSold(uint _commodityId) external onlyAuctioneer 
+    function markAsSold(uint _commodityId, uint _salePrice) external onlyAuctioneer 
     { //first ensures caller is auctioneer
         require(commodities[_commodityId].id != 0, "Commodity does not exist."); //checks if commodity exists by checking key in mapping(dictionary)
         require(!commodities[_commodityId].isSold, "Commodity already sold."); //checks if commodity is already sold
 
-
         commodities[_commodityId].isSold = true; //marks as sold if commodity exists and is not sold yet
+
+        //update seller totals automatically
+        totalSalesBySeller[commodities[_commodityId].seller] += _salePrice;
+        itemsSoldBySeller[commodities[_commodityId].seller] += 1;
 
         emit CommoditySold(_commodityId, msg.sender); //triggers event
     }
-
 
     function getCommodity(uint _id) external view returns (Commodity memory) 
     { //to get commodity information (structure)
         require(commodities[_id].id != 0, "Invalid commodity Id."); //checks for existance
         return commodities[_id]; //returns values of key (ID)
+    }
+    
+    //get all listings for a seller using sellerListings mapping - for front end
+    function getListingsBySeller(address _seller) external view returns (Commodity[] memory) {
+        uint[] memory listingIds = sellerListings[_seller]; //gets listing IDs of listings made by a seller
+        Commodity[] memory listings = new Commodity[](listingIds.length); //stores listings
+        for (uint i = 0; i < listingIds.length; i++) //gets commodity structure for each listing, stores in listing array
+        {
+            listings[i] = commodities[listingIds[i]];
+        }
+        return listings; //stored like Commodity("Wheat",100,5000), Commodity("Corn", 5, 200)
     }
 }
